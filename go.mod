9662af559fbc1de6881c8f03d140b669e00271d0
Richard Genthner <5654+moos3@users.noreply.github.com>
2020-05-12 17:35:56 -0400

    latest fixes with hot reloading :)

--- go.mod
+++ go.mod
@@ -3,28 +3,32 @@ module github.com/moos3/gin-rest-api
 go 1.12
 
 require (
-	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-contrib/logger v0.0.1
 	github.com/gin-gonic/gin v1.4.0
-	github.com/go-openapi/spec v0.19.2 // indirect
-	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/go-openapi/spec v0.19.7 // indirect
+	github.com/go-openapi/swag v0.19.9 // indirect
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.10
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.7 // indirect
-	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.14.3
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.2.0
-	github.com/swaggo/swag v1.6.2
+	github.com/swaggo/swag v1.6.5
 	github.com/ugorji/go v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586
-	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
-	golang.org/x/tools v0.0.0-20190823093517-aa644d2adf2a // indirect
+	github.com/urfave/cli v1.22.4 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/tools v0.0.0-20200512131952-2bc93b1c0c88 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,26 @@
-package main 
+package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
-	//"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/moos3/gin-rest-api/api"
 	"github.com/moos3/gin-rest-api/database"
+	"github.com/moos3/gin-rest-api/docs"
 	"github.com/moos3/gin-rest-api/lib/middlewares"
-	"github.com/gin-contrib/cors"
 
 	"github.com/gin-contrib/logger"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
-	"github.com/swaggo/files"
-	"github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
-	"github.com/moos3/gin-rest-api/docs"
-
+	_ "github.com/moos3/gin-rest-api/docs"
 )
 
 var region string
@@ -62,7 +63,7 @@ func main() {
 	docs.SwaggerInfo.Title = "Gin Rest API"
 	docs.SwaggerInfo.Description = "This is a simple gin rest api with authentication."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "api.guthnur.net"
+	docs.SwaggerInfo.Host = "api.example.com"
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
@@ -73,7 +74,7 @@ func main() {
 
 	// Add a logger middleware, which:
 	//   - Logs all requests, like a combined access and error log.
-	//   - Logs to stdout.	
+	//   - Logs to stdout.
 	app.Use(logger.SetLogger())
 
 	// Custom logger
@@ -82,23 +83,23 @@ func main() {
 		Logger()
 
 	app.Use(logger.SetLogger(logger.Config{
-		Logger:         &subLog,
-		UTC:            true,
-		SkipPath:       []string{"/skip"},
+		Logger:   &subLog,
+		UTC:      true,
+		SkipPath: []string{"/skip"},
 	}))
 
 	/*
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://localhost"},
-		AllowMethods: []string{"PUT", "PATCH", "GET"},
-		AllowHeaders: []string{"Origin"},
-		ExposeHeaders: []string{"Content-Lenght"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: []string{"https://localhost"},
+			AllowMethods: []string{"PUT", "PATCH", "GET"},
+			AllowHeaders: []string{"Origin"},
+			ExposeHeaders: []string{"Content-Lenght"},
+			AllowCredentials: true,
+			AllowOriginFunc: func(origin string) bool {
+				return origin == "https://github.com"
+			},
+			MaxAge: 12 * time.Hour,
+		}))
 	*/
 	app.Use(cors.Default())
 	app.Use(database.Inject(db))
@@ -106,7 +107,49 @@ func main() {
 	app.Use(middlewares.RevisionMiddleware())
 	app.Use(middlewares.JWTMiddleware())
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	rootPublic := app.Group("/")
+	{
+		swagURL, err := getBasePath(port)
+		if err != nil {
+			panic(err)
+		}
+		url := ginSwagger.URL(swagURL)
+		rootPublic.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+		rootPublic.GET("/healthcheck", healthCheck)
+		rootPublic.GET("/", message)
+	}
+
 	api.ApplyRoutes(app)
 
 	app.Run(":" + port)
+}
+
+func message(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "welcome to my nightmare"})
+}
+
+// useRootPath is our 'defaults' since most apps don't expect a v1/v2
+func healthCheck(c *gin.Context) {
+	c.JSON(200, map[string]string{
+		"status": "ok",
+		"env":    os.Getenv("ENV"),
+	})
+}
+
+func getBasePath(port string) (string, error) {
+	if os.Getenv("ENV") == "" {
+		return "", errors.New("no env found")
+	}
+	switch env := os.Getenv("ENV"); env {
+	case "development":
+		url := fmt.Sprintf("http://localhost:%s/swagger/doc.json", port)
+		return url, nil
+	default:
+		if os.Getenv("HOSTNAME") == "" {
+			return "", errors.New("no hostname was set, please use development")
+		}
+		hostname := os.Getenv("HOSTNAME")
+		url := fmt.Sprintf("http://%s/swagger/doc.json", hostname)
+		return url, nil
+	}
 }
